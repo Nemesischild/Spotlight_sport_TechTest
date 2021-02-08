fad30e33e8649d322fb87471d4ff329694a42215
Nemesischild <36448155+Nemesischild@users.noreply.github.com>
2021-02-08 12:56:38 +0000

    removed redundant comments

--- src/support/check/checkDateIsFuture.js
+++ src/support/check/checkDateIsFuture.js
@@ -1,11 +1,11 @@
 /**
- * Check if the given elements text is the same as the given text
+ * 
  * @param  {String}   selector       Element selector
  *
  */
 export default (selector) => {
     /**
-     * The command to execute on the browser object
+     * 
      * @type {String}
      */
     const element = $(selector);
